a7add99b1087edf3c5b369218ecc582ce5b9182b
edocabhi <niraula.a@gmail.com>
2016-05-10 13:11:45 +0545

    WCWD-V1

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('whereCanWeDanceCtrl', function($scope) {
+
+})
+   
+.controller('salsaCubanWorkshopCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+    
