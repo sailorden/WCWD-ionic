7f4d4bb2fb227cdd76049c4c3f05ce9a785acaca
Roan <rohankoid@gmail.com>
2016-06-04 12:22:23 +0200

    translation integrated, translation for menu items added.

--- www/app/core/core.modules.js
+++ www/app/core/core.modules.js
@@ -1,25 +1,25 @@
-(function(){
+(function () {
   'use strict';
 
-  angular.module('app.core', ['ionic', 'firebase']);
+  angular.module('app.core', ['ionic', 'firebase', 'pascalprecht.translate']);
 
   angular
     .module('app.core')
-    .run(['$ionicPlatform', '$rootScope', '$ionicLoading', function($ionicPlatform, $rootScope, $ionicLoading) {
-      $rootScope.showLoading = function(msg) {
+    .run(['$ionicPlatform', '$rootScope', '$ionicLoading', function ($ionicPlatform, $rootScope, $ionicLoading, $translate) {
+      $rootScope.showLoading = function (msg) {
         $ionicLoading.show({
           template: '<ion-spinner icon="lines" class="spinner-calm"></ion-spinner><br/><br/>' + msg
           //template: '<div class="loader"><svg class="circular"><circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/></svg></div><br>' + msg
         });
       };
 
-      $rootScope.hideLoading = function() {
+      $rootScope.hideLoading = function () {
         $ionicLoading.hide();
       };
 
       $rootScope.showLoading("Loading...");
 
-      $ionicPlatform.ready(function() {
+      $ionicPlatform.ready(function () {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
@@ -33,10 +33,51 @@
 
         $rootScope.hideLoading();
       });
+      var lang = window.localStorage['language'];
+      $rootScope.language = lang;
     }])
 
-    .config(function($firebaseRefProvider) {
+    .config(function ($firebaseRefProvider) {
       $firebaseRefProvider.registerUrl('https://wherecanwedance.firebaseio.com/');
+    })
+    // translate
+    .config(function ($translateProvider) {
+
+      $translateProvider.useStaticFilesLoader({
+        prefix: 'i18n/',
+        suffix: '.json'
+      });
+      $translateProvider
+        .registerAvailableLanguageKeys(['en', 'fr'], {
+          'fr_*': 'fr',
+          '*': 'en'
+        });
+
+      var lang = window.localStorage['language'];
+      if (lang) {
+        $translateProvider.preferredLanguage(lang);
+      }
+      else {
+        $translateProvider.preferredLanguage('en');
+        window.localStorage.setItem('language', 'en');
+      }
+      $translateProvider.fallbackLanguage("en");
     });
+
+  angular.module("pascalprecht.translate")
+    .factory("$translateStaticFilesLoader", ["$q", "$http", function (a, b) {
+      return function (c) {
+        if (!c || !angular.isString(c.prefix) || !angular.isString(c.suffix))
+          throw new Error("Couldn't load static files, no prefix or suffix specified!");
+        var d = a.defer();
+        return b({url: [c.prefix, c.key, c.suffix].join(""), method: "GET", params: ""})
+          .success(function (a) {
+            d.resolve(a)
+          })
+          .error(function () {
+            d.reject(c.key)
+          }), d.promise
+      }
+    }]);
 
 })();
--- www/app/home/whereCanWeDanceCtrl.js
+++ www/app/home/whereCanWeDanceCtrl.js
@@ -1,17 +1,22 @@
-(function(){
+(function () {
   'use strict';
 
   angular
     .module('app.home')
     .controller('whereCanWeDanceCtrl', whereCanWeDanceCtrl);
 
-  whereCanWeDanceCtrl.$inject = ['$scope', '$ionicModal'];
+  whereCanWeDanceCtrl.$inject = ['$scope', '$ionicModal', '$translate'];
 
-  function whereCanWeDanceCtrl($scope, $ionicModal){
+  function whereCanWeDanceCtrl($scope, $ionicModal, $translate) {
     $ionicModal.fromTemplateUrl('app/home/modal.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.modal = modal;
     });
+    $scope.change_language = function(lang){
+      $translate.use(lang);
+      $scope.language = lang;
+      window.localStorage.setItem('language', lang);
+    };
   }
 })();
